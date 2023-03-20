ab8124a561ddded028d2a532aad1e9c747ab9de4
Manjula <manjumanohar0704@gmail.com>
2023-03-21 00:39:38 +0530

    project completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,30 @@
 package com.driver;
 
 public class Main {
+    public static class Product {
 
+        public int product(int x, int y) {
+
+            return x*y;
+        }
+
+        public int product(int x, int y, int z) {
+
+            return x*y*z;
+        }
+
+        public double product(double x, double y) {
+
+            return x*y;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Product p = new Product();
+        System.out.println(p.product(8,9));
+        System.out.println(p.product(8,9,10));
+        System.out.println(p.product(8.4,9.67));
+    }
 }
